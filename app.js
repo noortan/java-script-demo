bab3e4274b44139553e26a02379e549f5bbb5917
NoortanAbuDarwe <>
2021-06-03 20:02:23 +0000

    new file

---
+++ app.js
@@ -0,0 +1,13 @@
+var heading = document.getElementsByClassName('accordion') ;
+console.log(heading[0]);
+
+
+var i ;
+for (i = 0; i < heading.length; i++){
+  heading[i].addEventListener('click'),function(){
+    var play = this.nextElementSibling ;
+
+    console.log(des);
+    
+  }
+}
